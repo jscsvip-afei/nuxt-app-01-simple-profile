ea5ca8df5646e1905ec33c5cefc44b87f79c6276
jscsvip-afei <lion_li2018@163.com>
2025-09-14 13:22:51 +0800

    chore: update build and generate scripts to use dotenv and silent logging; lock dependency versions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+if (process.env.VITE_ENV && !process.env.VITE_API_BASE_URL) {
+    throw new Error('请先于 .env.production 文件内，配置生产环境的 VITE_API_BASE_URL')
+}
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "@vueuse/motion/nuxt", "@nuxt/image"],
@@ -6,6 +10,7 @@ export default defineNuxtConfig({
   // 运行时配置，将环境变量暴露给客户端
   runtimeConfig: {
     public: {
+      env: process.env.VITE_ENV,
       apiBaseUrl: process.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000'
     }
   },
